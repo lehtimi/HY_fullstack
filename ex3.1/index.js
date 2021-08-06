ee69ac0279949d1e34641949d924a97230d161f4
lehtimi <inka.lehtimaki@gmail.com>
2021-08-06 14:47:08 +0300

    update scripts in package and edit index

--- ex3.1/index.js
+++ ex3.1/index.js
@@ -10,6 +10,7 @@ app.use(express.static('build'))
 app.use(cors())
 
 
+
 /*let people = [
     { 
       "id": 1,
@@ -39,10 +40,12 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
         response.json(person)
-    })
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -84,12 +87,49 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    people = people.filter(person => person.id !== id)
+/*
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    console.log(body)
 
-    response.status(204).end()
+    const person = new Person ({
+        id: request.params.id,
+        name: body.name,
+        number: body.number
+    })
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+
 })
+*/
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(400).send( {error: 'unknown endpoint'} )
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
