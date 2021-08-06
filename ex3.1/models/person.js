be08d08ba270458d95b0f341ecbbd317047000b8
lehtimi <inka.lehtimaki@gmail.com>
2021-08-06 15:36:56 +0300

    package.json testing for heroku

--- ex3.1/models/person.js
+++ ex3.1/models/person.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
+//const url = mongodb+srv://fullstack:hyfullaccess2021@cluster0.v2bpm.mongodb.net/phonebook?retryWrites=true&w=majority`
 console.log("connecting to", url)
-//`mongodb+srv://fullstack:hyfullaccess2021@cluster0.v2bpm.mongodb.net/phonebook?retryWrites=true&w=majority`
+
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(result => {
         console.log("connected to MongoDB")
